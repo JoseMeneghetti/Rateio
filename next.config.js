47e72d60e9c43e969d0b54c7e4dade4db1022f7a
joseMeneghetti <jrmp.dev@gmail.com>
2022-12-06 12:11:50 -0300

    teste again

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa")({
-  dest: "public",
-  skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
   register: true,
+  scope: "/app",
+  sw: "service-worker.js",
+  dest: "public",
 });
 
 module.exports = withPWA({
