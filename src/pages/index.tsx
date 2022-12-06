3bf28d56cb267eb043b67edf9166c599bf72f45a
joseMeneghetti <jrmp.dev@gmail.com>
2022-12-06 00:49:47 -0300

    fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,6 +35,9 @@ export default function Home({ data }: Props) {
     }
   }, []);
 
+  console.log(step2);
+  console.log(step3);
+
   return (
     <div className="max-w-[800px] mx-auto flex flex-col items-center">
       <div>
@@ -45,12 +48,11 @@ export default function Home({ data }: Props) {
       <div className="my-10 bg-yellow-500 bg-opacity-90 w-5/6 h-fit rounded-lg p-5">
         <div className="flex justify-start flex-row items-center h-full gap-4">
           <div className="flex flex-col w-full">
-            {step2 ||
-              (step3 && (
-                <span className="text-center lg:text-xl mb-2">
-                  Nome do Seu Rateio
-                </span>
-              ))}
+            {step2 && (
+              <span className="text-center lg:text-xl mb-2">
+                Nome do Seu Rateio
+              </span>
+            )}
             <input
               id="name"
               name="name"
@@ -62,15 +64,14 @@ export default function Home({ data }: Props) {
             ></input>
           </div>
 
-          {!step2 ||
-            (!step3 && (
-              <button
-                className="px-4 py-2 bg-blue-900 hover:bg-blue-700 text-yellow-500 rounded-lg font-jack lg:text-2xl h-fit"
-                onClick={() => setStep2(true)}
-              >
-                Continuar
-              </button>
-            ))}
+          {!step2 && (
+            <button
+              className="px-4 py-2 bg-blue-900 hover:bg-blue-700 text-yellow-500 rounded-lg font-jack lg:text-2xl h-fit"
+              onClick={() => setStep2(true)}
+            >
+              Continuar
+            </button>
+          )}
         </div>
       </div>
       {step2 && (
