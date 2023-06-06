8cf41b2c1cb5093ea8f7e9188e8badbf5bdf73fe
joseMeneghetti <jrmp.dev@gmail.com>
2023-06-06 19:15:08 -0300

    test

--- src/components/Modal/PasswordModal.tsx
+++ src/components/Modal/PasswordModal.tsx
@@ -1,21 +1,22 @@
 import { Transition } from "@headlessui/react";
 import axios from "axios";
 import { Fragment, useState } from "react";
+import Spinner from "../Spinner/Spinner";
 
 interface Props {
   isOpen: boolean;
   setIsOpen(value: boolean): void;
   id: number | null;
-  setLoading(value: boolean): void;
 }
 
-const PasswordModal = ({ isOpen, setIsOpen, id,setLoading }: Props) => {
+const PasswordModal = ({ isOpen, setIsOpen, id }: Props) => {
   const [password, setPassword] = useState("");
   const [errorMsg, setErrorMsg] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handleCheckPassword = () => {
     setLoading(true);
-    
+    debugger;
     axios
       .post("/api/password", {
         id: id,
@@ -30,7 +31,7 @@ const PasswordModal = ({ isOpen, setIsOpen, id,setLoading }: Props) => {
         }
       });
 
-      setLoading(false);
+    setLoading(false);
   };
 
   return (
@@ -49,28 +50,32 @@ const PasswordModal = ({ isOpen, setIsOpen, id,setLoading }: Props) => {
 
       <div className="fixed inset-0 overflow-y-auto">
         <div className="flex min-h-full flex-col justify-center items-center content-center">
-          <div className="flex min-h-full items-center justify-center p-4 text-center">
-            <div className="flex flex-col gap-4">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                placeholder="Senha"
-                className="bg-theme-4 py-2 px-4 rounded lg:text-2xl placeholder:text-black w-full font-bold"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
+          {loading ? (
+            <Spinner customClass="fill-black" size="h-20 w-20" />
+          ) : (
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <div className="flex flex-col gap-4">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder="Senha"
+                  className="bg-theme-4 py-2 px-4 rounded lg:text-2xl placeholder:text-black w-full font-bold"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="flex justify-center p-4">
+                <button
+                  className="px-4 py-2 bg-theme-5 hover:bg-theme-2 text-theme-6 rounded-lg text-3xl h-fit flex items-center gap-3 text-theme-4"
+                  onClick={() => handleCheckPassword()}
+                >
+                  Entrar
+                </button>
+              </div>
             </div>
-            <div className="flex justify-center p-4">
-              <button
-                className="px-4 py-2 bg-theme-5 hover:bg-theme-2 text-theme-6 rounded-lg text-3xl h-fit flex items-center gap-3 text-theme-4"
-                onClick={() => handleCheckPassword()}
-              >
-                Entrar
-              </button>
-            </div>
-          </div>
+          )}
           {errorMsg && (
             <span className="text-red-800 font-bold py-4">
               Senha Incorreta!
